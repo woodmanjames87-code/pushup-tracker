f98d0141c540944f5de1395445097743f4c4fdd7
woodmanjames87-code <woodman.james87@gmail.com>
2026-02-11 09:17:44 -0700

    Leaderboard 4.6.4.1

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = 'v4.6.4'; // Increment this to update the app
+const VERSION = 'v4.6.4.1'; // Increment this to update the app
 const CACHE_NAME = `workout-${VERSION}`; 
 
 const ASSETS = [
