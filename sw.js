1f74a5bea5c6456f9e74bd8dd3c4ac7e8205aaf2
woodmanjames87-code <woodman.james87@gmail.com>
2026-02-14 11:03:14 -0700

    css fix

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = 'v4.7.1.2'; // Increment this to update the app
+const VERSION = 'v4.7.1.3'; // Increment this to update the app
 const CACHE_NAME = `workout-${VERSION}`; 
 
 const ASSETS = [
