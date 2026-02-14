07bb1b3c763b14d7402ffb35a1185a6919aa71a3
woodmanjames87-code <woodman.james87@gmail.com>
2026-02-14 10:37:18 -0700

    css fix after split

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = 'v4.7.1.0'; // Increment this to update the app
+const VERSION = 'v4.7.1.1'; // Increment this to update the app
 const CACHE_NAME = `workout-${VERSION}`; 
 
 const ASSETS = [
