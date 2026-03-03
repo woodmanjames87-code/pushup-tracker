e3b191f751c7634b2a3bdd6d048dc2ddfb46039e
woodmanjames87-code <woodman.james87@gmail.com>
2026-03-02 23:36:16 -0700

    fix selectedEditDate variable

--- js/ui.js
+++ js/ui.js
@@ -271,7 +271,7 @@ function renderEditList() {
 
 window.deleteSet = (i) => {
     const data = window.loadData();
-    const dateKey = window.selectedEditDate();
+    const dateKey = window.selectedEditDate;
     const exercise = window.currentExercise();
 
     if (data[dateKey] && data[dateKey][exercise]) {
