814aef9c4c4076b1e45825cb8504b4023dada87e
James Woodman <woodman.james87@gmail.com>
2026-07-18 23:52:53 -0600

    style: adjust padding and margin for improved layout consistency

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "v5.0.6.5"; // Increment this to update the app
+const VERSION = "v5.0.6.6"; // Increment this to update the app
 const CACHE_NAME = `DailyGrind-${VERSION}`;
 
 const ASSETS = [
