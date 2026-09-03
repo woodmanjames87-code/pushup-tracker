70a0d65bfafcc47904cc46dc74c5210daa033667
James Woodman <woodman.james87@gmail.com>
2026-09-03 00:40:37 -0600

    fix header and page padding

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "v5.0.6.10"; // Increment this to update the app
+const VERSION = "v5.0.6.11"; // Increment this to update the app
 const CACHE_NAME = `DailyGrind-${VERSION}`;
 
 const ASSETS = [
